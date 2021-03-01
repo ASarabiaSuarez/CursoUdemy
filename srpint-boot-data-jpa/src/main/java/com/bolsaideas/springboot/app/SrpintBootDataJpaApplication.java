ce1a227c4c5a8c8c88e072db5655b21e0103976a
Alberto Sarabia <alberto.sarabia@edosoft.es>
2021-03-01 07:22:48 +0000

    agregando la parte del curso de los DAO

---
+++ srpint-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/SrpintBootDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.bolsaideas.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpintBootDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpintBootDataJpaApplication.class, args);
+	}
+
+}
